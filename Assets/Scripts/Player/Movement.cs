fab69ffc092fe5eb054ccd4231bc55a0a1fc431e
fkeyzuwu <fkeyzuwu@gmail.com>
2021-09-09 15:59:02 +0300

    sss
    bsaic script templates

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public void Move(Vector3 direction)
+    {
+
+    }
+}
